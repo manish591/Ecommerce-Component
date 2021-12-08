57126ce2147d63ba28e33eb9f3ad7992258150f9
Manish <manishdevrani777@gmail.com>
2021-12-08 15:10:51 +0530

    feat- updated cart total on changing quantity

--- js/script.js
+++ js/script.js
@@ -7,80 +7,89 @@ if(document.readyState === 'loading') {
 
 function ready() {
     const addToCartButton = document.querySelectorAll('.add-to-cart');
+    const increasePrice = document.querySelectorAll('.increase-price');
 
     addToCartButton.forEach(button => {
         button.addEventListener('click', addItmesToCart);
     });
-}
 
+    increasePrice.forEach(button => {
+        button.addEventListener('click', increaseCartPrice);
+    })
+}
 
 
 function addItmesToCart(e) {
-    const emptyCartMessage = document.querySelector('.empty-cart-message');
-    e.target.style.backgroundColor = 'black';
+    let button = e.target;
+    let emptyCartMessage = document.querySelector('.empty-cart-message');
     emptyCartMessage.classList.add('hide');
-    e.target.innerHTML = `<div class="in-cart-description"><img src="/STARTER-FILES/images/check.svg"> <p>In Cart</p></div>`;
     
-    const yourCartItemsSection = document.querySelector('.your-cart-items-section');
-    let element = e.target.parentNode;
-    let div = document.createElement('div');
-    div.classList.add('cart-item');
-    div.innerHTML = generateHTML(element.dataset.name, element.dataset.price, element.dataset.image);
-    yourCartItemsSection.appendChild(div);
-    
-    const increasePriceButton = document.querySelectorAll('.increase-price');
-    const decreasePriceButton  = document.querySelectorAll('.decrease-price');
-
-    increasePriceButton.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            let price = Number(e.target.parentElement.parentElement.parentElement.parentElement.querySelector('.price').innerText.replace('$', ''));
-            const totalRecipePrice = e.target.parentElement.parentElement.parentElement.querySelector('.updated-price');
-            let item = e.target.parentElement.previousElementSibling;
-            let value = Number(item.dataset.id);
-            item.dataset.id = value + 1;
-            item.innerText = item.dataset.id;
-            updateTotalPrice(price, item.dataset.id, totalRecipePrice);
-        })
-    })
-
-    decreasePriceButton.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            let price = Number(e.target.parentElement.parentElement.parentElement.parentElement.querySelector('.price').innerText.replace('$', ''));
-            const totalRecipePrice = e.target.parentElement.parentElement.parentElement.querySelector('.updated-price');
-            let item = e.target.parentElement.nextElementSibling;
-            let value = Number(item.dataset.id);
-            item.dataset.id = value + 1;
-            item.innerText = item.dataset.id;
-            updateTotalPrice(price, item.dataset.id, totalRecipePrice);
-        })
-    })
-}
-
-
-function updateTotalPrice(price, quantity, element) {
-    let currentTotal =  price * quantity;
-    element.innerText = `$${(currentTotal).toFixed(2)}`;
+    let element = button.parentElement;
+    let title = element.querySelector('.name').innerText;
+    let price = element.querySelector('.price').innerText;
+    let imageSrc = element.previousElementSibling.querySelector('.recipe-img').src;
+    generateHTML(title, price, imageSrc);
 }
 
 function generateHTML(name, price, image) {
-    return `<div class="image">
-              <img class="cart-recipe-img" src="/STARTER-FILES/images/${image}.png" alt="French Fries">
-              <p class="recipe-count">1</p>
-            </div>
-            <div class="cart-description">
-                <p class="name">${name}</p>
-                <p class="price">${price}</p>
-                <div class="select-area">
-                    <div class="select-quantity">
-                        <button class="decrease-price">
-                            <img src="/STARTER-FILES/images/chevron.svg" alt="" class="minus">
-                        </button>
-                        <p class="quantity" data-id="1">1</p>
-                        <button class="increase-price">
-                            <img src="/STARTER-FILES/images/chevron.svg" alt="" class="plus">
-                        </button>
+    let div = document.createElement('div');
+    div.classList.add('cart-item');
+    let yourCartContainer = document.querySelector('.your-cart-items-section');
+    let allItemsNames = yourCartContainer.querySelectorAll('.name');
+    for(let i = 0; i < allItemsNames.length; i++) {
+        if(allItemsNames[i].innerText === name) {
+            alert('Item is already added to the cart!');
+            return;
+        }
+    }
+    let content =  `<div class="image">
+                      <img class="cart-recipe-img" src=${image} alt="French Fries">
+                      <p class="recipe-count">1</p>
                     </div>
-                    <h2 class="updated-price">$0.00</h2>
-                </div>
-            </div>`
+                    <div class="cart-description">
+                        <p class="name">${name}</p>
+                        <p class="price">${price}</p>
+                        <div class="select-area">
+                            <div class="select-quantity">
+                                <button class="decrease-price update-price-value">
+                                    <img src="/STARTER-FILES/images/chevron.svg" alt="" class="minus">
+                                </button>
+                                <p class="quantity" data-id="1" data-value=${price}>1</p>
+                                <button class="increase-price update-price-value">
+                                    <img src="/STARTER-FILES/images/chevron.svg" alt="" class="plus">
+                                </button>
+                            </div>
+                            <h2 class="updated-price">${price}</h2>
+                        </div>
+                    </div>`;
+    div.innerHTML = content;               
+    yourCartContainer.appendChild(div);
+    let increasePriceButton = div.querySelector('.increase-price');
+    let decreasePriceButton = div.querySelector('.decrease-price');
+    increasePriceButton.addEventListener('click', increaseCartPrice);
+    decreasePriceButton.addEventListener('click', decreaseCartPrice);           
+}
+
+function increaseCartPrice(event) {
+    let button = event.target;
+    let parent = button.parentElement.parentElement;
+    let price = parent.parentElement.parentElement.querySelector('.price').innerText.replace('$', '');
+    let totalPriceForCart = parent.parentElement.querySelector('.updated-price');
+    let quantityToChange = parent.querySelector('.quantity');
+    quantityToChange.innerText = Number(quantityToChange.innerText) + 1;
+    totalPriceForCart.innerText = updateTotalPrice(price, quantityToChange.innerText);
+}
+
+function decreaseCartPrice(event) {
+    let button = event.target;
+    let parent = button.parentElement.parentElement;
+    let price = parent.parentElement.parentElement;
+    console.log(price);
+    let quantityToChange = parent.querySelector('.quantity');
+    quantityToChange.innerText = Number(quantityToChange.innerText) - 1;
+}
+
+function updateTotalPrice(price, quantity) {
+    let currentTotal =  Number(price) * Number(quantity);
+    return `$${(currentTotal).toFixed(2)}`;
 }
